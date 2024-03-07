2c5be43dda105e1e7eddded1aedafe72380a896f
elirrgn <elir.arge@gmail.com>
2024-03-07 12:03:10 +0100

    Administrator Check

--- script.sh
+++ script.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 cd /
 
-#controllo se la cartella di backup esiste se non esiste la creo
-if [ -e "backup" ]
-    then
-        cd backup
-    else
-        #creazione cartella, richiesti permessi da ammistratore
-        sudo mkdir backup
+# Controlla se l'utente è root o ha i privilegi di amministratore
+
+if [[ $(id -u) -eq 0 ]]; then
+    if [ -e "backup" ]
+        then
+            cd backup
+        else
+            #creazione cartella, richiesti permessi da ammistratore
+            sudo mkdir backup
+    fi
+
+else
+    if [ -e "backup" ]
+        then
+            cd backup
+        else
+            echo "Richiesti i diritti amministratore per creare la cartella backup"
+    fi
 fi
+
+#controllo se la cartella di backup esiste se non esiste la creo
 
